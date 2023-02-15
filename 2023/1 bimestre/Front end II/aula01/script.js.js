0bc1378d5dde14e4c7b80c93e19c2a9a28fe647c
Fernando Santos <fernandos245@hotmail.com>
2023-02-15 17:30:36 -0300

    aula02frontII

---
+++ 2023/1 bimestre/Front end II/aula01/script.js.js
@@ -0,0 +1,8 @@
+var array = [1, 2, 4, 8]
+var soma = 0
+
+
+for (const iterator of array) {
+soma += iterator
+}
+console.log(soma/4)
---
+++ 2023/1 bimestre/Front end II/testeA2.js
@@ -0,0 +1,18 @@
+// 1
+
+// let idade = parseInt(prompt("Introduza sua idade")); 
+// if(idade>=18){ 
+// alert("É maior de idade"); 
+// }else{ 
+// alert("É menor de idade"); 
+// } 
+
+// 1
+
+// 2
+
+parseInt(Math.random()*3+1)
+
+console.log(parseInt(Math.random()*3+1))
+
+// 2
